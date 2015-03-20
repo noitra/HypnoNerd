3833cf536ad7d3cdec0cc4e764cf4804199ee530
Noitra <kapiau@gmail.com>
2015-03-20 13:50:55 -0300

    Initial Commit

---
+++ HypnoNerd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HypnoNerd
+//
+//  Created by Gabriel Oliva de Oliveira on 3/20/15.
+//  Copyright (c) 2015 Gabriel Oliva de Oliveira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
